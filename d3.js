d7784f806805e9fb9c815a588562c4586ea1400f
Igor Victorovich <74201424+Igor0606Victorovich@users.noreply.github.com>
2023-04-15 18:20:10 +0500

    Add files via upload

--- d3.js
+++ d3.js
@@ -20,30 +20,55 @@ var bar = graph
  return "translate(0," + i * barHeight + ")";
 });
 
+
 bar
 .append("rect")
-.attr("width", function(d) {
-return d * scaleFactor;
+.attr("width", function(d, i) {
+	if (i === 0) {
+		return d * scaleFactor;
+	} else if (i === data.length - 1) {
+		return d * scaleFactor - 100;
+	} else {
+		return d * scaleFactor * 10;
+	}
+
 	})
-.attr("height", barHeight - 1);
+.attr("height", barHeight - 1)
+.attr("fill", function(d, i) {
+	if (i === 0) {
+		return "white";
+	} else if (i === data.length - 1) {
+		return "#20B2AA";
+	} else {
+		return "#32CD32"
+	}
+});
 		 
 bar.append("text")
-.attr("x", function(d) { 
-return (d*scaleFactor); 
+
+.attr("x", function(d, i) { 
+	if (i === 0){
+return (d*scaleFactor * 0.5);
+} else if (i === data.length - 1) {
+return (d*scaleFactor * 0.5);
+} else return (d*scaleFactor * 12);
   })
+
 .attr("y", barHeight / 2)
 .attr("dy", ".35em")
+
+.attr("fill", function(d, i){
+	return i === 0 ? "black" : "white";
+})
+.attr("text-anchor", function(d, i) {
+	if (i === 0) {
+		return "middle";
+	} else if (i === data.length - 1) {
+		return "middle";
+	} else {
+		return "right"
+	}
+})
 .text(function(d) { return d; 
  });
 
-
-// var bar1 = graph
-// .select("g")	 
-// .data(data)
-
-// bar1
-// .append("rect")
-// .attr("width", function(d) {
-//  return d * scaleFactor *10;
-//  })
-// .style("color", "white")
